8159708166482706fc8bd63c86292fde69ce61a3
Raphael <veauville.raphael@gmail.com>
2021-08-22 18:53:48 +0200

    Fix and add tsconfig.json

--- src/templates/typescript/src/interactions/buttons/button.ts
+++ src/templates/typescript/src/interactions/buttons/button.ts
@@ -17,7 +17,7 @@ export class ButtonTest extends Button {
         await button.reply({ content: "You have clicked on **primary** button !" });
         break;
       case "secondaryId":
-        awaitnbutton.reply({ content: "You have clicked on **secondary** button !" });
+        await button.reply({ content: "You have clicked on **secondary** button !" });
         break;
       case "successId":
         await button.reply({ content: "You have clicked on **success** button !" });
